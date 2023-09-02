cb310c433dfd8d70efcd3acb4d853d7c47c8ff00
nerdai <92402603+nerdai@users.noreply.github.com>
2023-09-02 17:54:50 -0400

    add unit test for spatial bn (#1)
    * add unit test
    
    * change version of pytorch

--- tests/test_batch_normalization.py
+++ tests/test_batch_normalization.py
@@ -3,7 +3,10 @@ from unittest import TestCase
 import numpy as np
 import torch
 
-from dlpr_code.batch_normalization import TemporalBatchNormalization
+from dlpr_code.batch_normalization import (
+    SpatialBatchNormalization,
+    TemporalBatchNormalization,
+)
 from dlpr_code.constants import SMOOTHING_TERM
 
 
@@ -25,6 +28,60 @@ class BatchNormalizationTest(TestCase):
             / (batch_std + SMOOTHING_TERM),
             torch.ones(model_dim),
         )
+        self.assertAlmostEqual(
+            torch.norm(results - expected),
+            0.0,
+            None,
+            "result not almost equal to expected",
+            1e-5,
+        )
+
+    def test_spatial_bn(self):
+        height = 4
+        width = 4
+        channels = 3
+        spatial_bn = SpatialBatchNormalization(num_channels=channels)
+        sample = torch.cat(
+            (
+                torch.zeros(channels, height, width).unsqueeze(0),
+                torch.ones(channels, height, width).unsqueeze(0),
+                torch.ones(channels, height, width).unsqueeze(0) * 2,
+                torch.ones(channels, height, width).unsqueeze(0) * 3,
+                torch.ones(channels, height, width).unsqueeze(0) * 4,
+            )
+        )
+        results = spatial_bn(sample)
+
+        # expected
+        batch_mean = 2.0
+        batch_std = np.sqrt(2.0)
+        expected = torch.cat(
+            (
+                (
+                    torch.zeros(channels, height, width).unsqueeze(0)
+                    - batch_mean
+                )
+                / (batch_std + SMOOTHING_TERM),
+                (torch.ones(channels, height, width).unsqueeze(0) - batch_mean)
+                / (batch_std + SMOOTHING_TERM),
+                (
+                    torch.ones(channels, height, width).unsqueeze(0) * 2
+                    - batch_mean
+                )
+                / (batch_std + SMOOTHING_TERM),
+                (
+                    torch.ones(channels, height, width).unsqueeze(0) * 3
+                    - batch_mean
+                )
+                / (batch_std + SMOOTHING_TERM),
+                (
+                    torch.ones(channels, height, width).unsqueeze(0) * 4
+                    - batch_mean
+                )
+                / (batch_std + SMOOTHING_TERM),
+            )
+        )
+
         self.assertAlmostEqual(
             torch.norm(results - expected),
             0.0,
